87eaa5f9674b0da0118d0abb1c14e98a2e4a7e96
felix <felix.mueller0998@gmx.de>
2025-07-26 12:59:35 +0200

    Adds repositories for Member and Loan Entities

---
+++ Domain/Loans/ILoanRepository.cs
@@ -0,0 +1,10 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Domain.Loans;
+public interface ILoanRepository
+{
+    Task<List<Loan>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<Maybe<Loan>> GetByIdAsync(LoanId id, CancellationToken cancellationToken);
+
+    void Add(Loan book);
+}
