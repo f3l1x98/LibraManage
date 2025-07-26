edf5e0a4130095b1e047b8f40323b4b0ef979cd3
felix <felix.mueller0998@gmx.de>
2025-07-26 18:09:06 +0200

    Adds Dtos and AutoMapper Profiles for Member and Loan

--- LibraManage/Extensions/MapperExtension.cs
+++ LibraManage/Extensions/MapperExtension.cs
@@ -7,6 +7,8 @@ public static class MapperExtension
     public static IServiceCollection RegisterAutoMapperProfiles(this IServiceCollection services)
     {
         services.AddAutoMapper(typeof(BookProfile));
+        services.AddAutoMapper(typeof(MemberProfile));
+        services.AddAutoMapper(typeof(LoanProfile));
         return services;
     }
 }
