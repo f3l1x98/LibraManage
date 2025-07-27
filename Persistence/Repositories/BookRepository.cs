69d632f4bd8f24647ac15058a414e2ace2c08260
felix <felix.mueller0998@gmx.de>
2025-07-27 16:21:45 +0200

    Fixes Books not containing loans

--- Persistence/Repositories/BookRepository.cs
+++ Persistence/Repositories/BookRepository.cs
@@ -1,9 +1,26 @@
-﻿using Domain.Books;
+﻿using CSharpFunctionalExtensions;
+using Domain.Books;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence.Repositories;
 public class BookRepository : Repository<Book, BookId>, IBookRepository
 {
     public BookRepository(ApplicationDbContext dbContext) : base(dbContext)
     {
+    }
+
+    public override Task<Maybe<Book>> GetByIdAsync(BookId id, CancellationToken cancellationToken = default)
+    {
+        return _dbContext.Set<Book>()
+            .Include(b => b.LoanedCopies)
+            .SingleOrDefaultAsync(e => e.Id == id, cancellationToken)
+            .AsMaybe();
+    }
+
+    public virtual Task<List<Book>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return _dbContext.Set<Book>()
+            .Include(b => b.LoanedCopies)
+            .ToListAsync(cancellationToken);
     }
 }
